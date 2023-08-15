10429baaad18c27fec29a80fd079d4c8a00c46f5
Juan Hoyos <juanhoyos1347@gmail.com>
2023-08-14 20:56:23 -0500

    bucles

---
+++ src/main/scala/Blucles.scala
@@ -0,0 +1,23 @@
+object Bucles {
+
+  // Bucle for
+  println("Bucle for:")
+  for (i <- 1 to 5) {
+    println(s"Valor de i: $i")
+  }
+
+  // Bucle while
+  println("\nBucle while:")
+  var j = 0
+  while (j < 5) {
+    println(s"Valor de j: $j")
+    j += 1
+  }
+
+  // Bucle do-while
+  while
+    var x: Int = 0;
+    x >=0;
+  do print(".");
+  
+}
